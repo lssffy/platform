cb26c09ca4787f99ef67e9e4be5af9575c1fd85d
lssqqy <lssffy@163.com>
2024-03-13 14:24:05 +0800

    eight commit

---
+++ src/main/java/com/sp/demo/UserApplication.java
@@ -0,0 +1,18 @@
+package com.sp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : lssffy
+ * @Description :
+ * @date : 2024/3/12 22:52
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+
+}
